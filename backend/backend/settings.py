c3861636ed1c0d897e6e9eec7fc5d962e2df06c5
Sahil Pawar <sahilpawar@Sahils-MacBook-Air.local>
2026-06-13 22:44:49 +0530

    fix: add STATIC_ROOT and WhiteNoise middleware for production

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -33,6 +33,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -97,6 +98,7 @@ USE_TZ = True
 
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
